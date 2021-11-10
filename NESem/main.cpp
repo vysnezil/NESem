05ef06d1d280250b0f0fb9dc13cc8f3bc7fffcac
vysnezil <mmamma151@gmail.com>
2021-11-10 01:37:22 +0100

    Add image, move roms folder

--- NESem/main.cpp
+++ NESem/main.cpp
@@ -6,17 +6,11 @@
 int main() {
 	Logger logger;
 
-	Cartridge card(".\\roms\\nestest.nes");
+	Cartridge card("..\\roms\\nestest.nes");
 	Bus bus;
 	bus.loadCartridge(&card);
 	bus.reset();
 	bus.cpu.jump(0xc000);
-
-	/*for (size_t i = 0x1FFF; i < 32768; i++)
-	{
-		logger.logHex(bus.read(i));
-	}*/
-	
 
 	while (1) {
 		bus.clock();
